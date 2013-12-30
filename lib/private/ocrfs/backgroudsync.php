966b3a6e55ee1206f804ada8f6a29827666f03e8
LittleKita <clange@nw7.de>
2013-12-30 20:38:04 +0100

    Backgroud ServerReplicationSync

---
+++ lib/private/ocrfs/backgroudsync.php
@@ -0,0 +1,37 @@
+<?php
+namespace OC\OCRFS;
+
+class BackgroudSync extends \OC\BackgroundJob\TimedJob {
+    public function __construct() {
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+        error_log("BackgroudSync");
+    }
+    
+    protected function run($argument) {
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+        error_log("BackgroudSync run");
+    }
+}
